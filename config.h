e26ee9d3905adf302b3a96686b55b12125d8f8ec
Andrew Hill <andrew@andrewkhill.com>
2021-05-25 22:33:44 -0500

    Change font to match new st font

--- config.h
+++ config.h
@@ -12,9 +12,9 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          =  {"Inconsolata for Powerline:size=14",
-                                        "JoyPixels"};
-static const char dmenufont[]       = "Inconsolata for Powerline:size=14";
+static const char *fonts[]          =  {"MesloLGS NF:size=8",
+                                        "JoyPixels:size=10"};
+static const char dmenufont[]       = "MesloLGS NF:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -46,10 +46,10 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
+	{ "[D]",      deck },
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
-	{ "[D]",      deck },
 };
 
 /* key definitions */
